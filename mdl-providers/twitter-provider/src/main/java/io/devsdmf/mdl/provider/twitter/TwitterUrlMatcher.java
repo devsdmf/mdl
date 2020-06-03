94dfa8b54b1ea4701a4817289ebdec3ecfe129b2
devsdmf <devsdmf@gmail.com>
2020-06-03 03:20:54 -0300

    Implemented twitter URL matcher

---
+++ mdl-providers/twitter-provider/src/main/java/io/devsdmf/mdl/provider/twitter/TwitterUrlMatcher.java
@@ -0,0 +1,21 @@
+package io.devsdmf.mdl.provider.twitter;
+
+import io.devsdmf.mdl.matcher.UrlMatcher;
+
+import java.net.URI;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TwitterUrlMatcher implements UrlMatcher {
+
+    public static final String PATTERN =
+            "^([http(s):\\/\\/(www\\.)twitter.com]{2,256})\\/(\\b[-a-zA-Z0-9@:%_\\+.~#?&=]*)\\/status(?:es)*\\/(\\d+).+$";
+
+    @Override
+    public Boolean match(URI url) {
+        Pattern p = Pattern.compile(PATTERN,Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(url.toString());
+
+        return m.matches();
+    }
+}

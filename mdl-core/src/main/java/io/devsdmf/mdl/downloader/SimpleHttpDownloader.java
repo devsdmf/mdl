2868a9a065ed0b0d28672640ae8ed064d897f6de
devsdmf <devsdmf@gmail.com>
2020-06-16 04:35:28 -0300

    Improved SimpleHttpDownloader with file extension resolution

--- mdl-core/src/main/java/io/devsdmf/mdl/downloader/SimpleHttpDownloader.java
+++ mdl-core/src/main/java/io/devsdmf/mdl/downloader/SimpleHttpDownloader.java
@@ -4,20 +4,24 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SimpleHttpDownloader implements Downloader {
 
-    public File download(URL fileUrl, String outputFileName) throws IOException {
+    public File download(URL fileUrl, String outputFileName) throws IOException, DownloaderException {
 //        HttpURLConnection httpConnection = (HttpURLConnection) fileUrl.openConnection();
 
         // getting the file size
 //        httpConnection.setRequestMethod("HEAD");
 //        Long fileSize = httpConnection.getContentLengthLong();
 
+        String ext = getFileExtension(fileUrl.getPath());
+        String outputFile = outputFileName + "." + ext;
+
         BufferedInputStream in = new BufferedInputStream(fileUrl.openStream());
-        FileOutputStream out = new FileOutputStream(outputFileName);
+        FileOutputStream out = new FileOutputStream(outputFile);
 
         byte[] buffer = new byte[1024];
         int bytesRead;
@@ -29,6 +33,17 @@ public class SimpleHttpDownloader implements Downloader {
         in.close();
         out.close();
 
-        return new File(outputFileName);
+        return new File(outputFile);
+    }
+
+    private static String getFileExtension(String path) throws DownloaderException {
+        Pattern p = Pattern.compile("\\.([a-zA-Z0-9]+)");
+        Matcher m = p.matcher(path);
+
+        if (!m.find() || m.groupCount() == 0) {
+            throw new DownloaderException("Could not resolve file extension for path: " + path);
+        }
+
+        return m.group(1);
     }
 }
